ec5fe55787cc4f6a5a4a302cef2dba0a23a812ea
Vipul-Bhangale <vipulb1575@gmail.com>
2021-09-20 11:18:19 +0530

    Branch Test

--- testjava/src/testjava/reportft.java
+++
@@ -1,7 +0,0 @@
-package testjava;
-
-public class reportft {
-	public static void main(String[] args) {
-		System.out.println("Branch Test 2");
-	}
-}
--- testjava/src/testjava/userloginft.java
+++
@@ -1,7 +0,0 @@
-package testjava;
-
-public class userloginft {
-	public static void main(String[] args) {
-		System.out.println("Branch Test");
-	}
-}
